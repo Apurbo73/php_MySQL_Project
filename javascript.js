a697d031fd1f7a4321472dea6e99ad18f8800a62
Apurbo73 <apurbodebnath895@gmail.com>
2022-06-22 01:31:03 +0600

    Xiomi

--- javascript.js
+++ javascript.js
@@ -4,5 +4,7 @@ document.getElementById('Xiomi-plus').addEventListener('click',function(){
 var caseInput= document.getElementById('Xiomi-number');
 var caseNumber= caseInput.value;
 caseInput.value=parseInt(caseNumber)+1;
-console.log(caseInput);
+
+
+
 })
