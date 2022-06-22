7f0ae08e0d45cbb8df0d747094e4f07f55dd883f
Apurbo73 <apurbodebnath895@gmail.com>
2022-06-22 14:15:47 +0600

    add cart button

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 //smart phone 
-
+//Xiomi
 document.getElementById('Xiomi-plus').addEventListener('click',function(){
 var caseInput= document.getElementById('Xiomi-number');
 var caseNumber= caseInput.value;
@@ -8,3 +8,272 @@ caseInput.value=parseInt(caseNumber)+1;
 
 
 })
+
+
+
+
+//Iphone
+
+document.getElementById('Iphone-plus').addEventListener('click',function(){
+    var caseInput= document.getElementById('Iphone-number');
+    var caseNumber= caseInput.value;
+    caseInput.value=parseInt(caseNumber)+1;
+    
+    
+    
+    })
+
+
+
+//Samsung
+
+document.getElementById('Samsung-plus').addEventListener('click',function(){
+    var caseInput= document.getElementById('Samsung-number');
+    var caseNumber= caseInput.value;
+    caseInput.value=parseInt(caseNumber)+1;
+    
+    
+    
+    })
+
+
+
+
+
+
+
+    //Laptops
+
+    //Lenovo
+
+    document.getElementById('Lenovo-plus').addEventListener('click',function(){
+        var caseInput= document.getElementById('Lenovo-number');
+        var caseNumber= caseInput.value;
+        caseInput.value=parseInt(caseNumber)+1;
+        
+        
+        
+        })
+
+
+
+
+    //Dell
+
+    document.getElementById('Dell-plus').addEventListener('click',function(){
+        var caseInput= document.getElementById('Dell-number');
+        var caseNumber= caseInput.value;
+        caseInput.value=parseInt(caseNumber)+1;
+        
+        
+        
+        })
+
+
+    // Mac
+
+    document.getElementById('Mac-plus').addEventListener('click',function(){
+        var caseInput= document.getElementById('Mac-number');
+        var caseNumber= caseInput.value;
+        caseInput.value=parseInt(caseNumber)+1;
+        
+        
+        
+        })
+
+
+
+//Shoes corner
+
+//Sply
+
+document.getElementById('Sply-plus').addEventListener('click',function(){
+    var caseInput= document.getElementById('Sply-number');
+    var caseNumber= caseInput.value;
+    caseInput.value=parseInt(caseNumber)+1;
+    
+    
+    
+    })
+
+
+
+// Nike
+
+
+document.getElementById('Nike-plus').addEventListener('click',function(){
+    var caseInput= document.getElementById('Nike-number');
+    var caseNumber= caseInput.value;
+    caseInput.value=parseInt(caseNumber)+1;
+    
+    
+    
+    })
+
+
+
+
+
+
+
+//Red Airmax
+
+document.getElementById('Red-plus').addEventListener('click',function(){
+    var caseInput= document.getElementById('Red-number');
+    var caseNumber= caseInput.value;
+    caseInput.value=parseInt(caseNumber)+1;
+    
+    
+    
+    })
+
+
+
+    //Bags corner
+
+ //Red-bag
+document.getElementById('Red-bag-plus').addEventListener('click',function(){
+    var caseInput= document.getElementById('Red-bag-number');
+    var caseNumber= caseInput.value;
+    caseInput.value=parseInt(caseNumber)+1;
+    
+    
+    
+    })
+
+
+// Blue bag
+document.getElementById('Blue-bag-plus').addEventListener('click',function(){
+    var caseInput= document.getElementById('Blue-bag-number');
+    var caseNumber= caseInput.value;
+    caseInput.value=parseInt(caseNumber)+1;
+    
+    
+    
+    })
+
+
+  // Black bag
+    document.getElementById('Black-bag-plus').addEventListener('click',function(){
+        var caseInput= document.getElementById('Black-bag-number');
+        var caseNumber= caseInput.value;
+        caseInput.value=parseInt(caseNumber)+1;
+        
+        
+        
+        })
+
+
+
+
+
+        //Foods corner
+// Burger
+document.getElementById('Burger-plus').addEventListener('click',function(){
+    var caseInput= document.getElementById('Burger-number');
+    var caseNumber= caseInput.value;
+    caseInput.value=parseInt(caseNumber)+1;
+    
+    
+    
+    })
+
+
+//Combo
+document.getElementById('Combo-plus').addEventListener('click',function(){
+    var caseInput= document.getElementById('Combo-number');
+    var caseNumber= caseInput.value;
+    caseInput.value=parseInt(caseNumber)+1;
+    
+    
+    
+    })
+
+
+
+ //Salad
+    document.getElementById('Salad-plus').addEventListener('click',function(){
+        var caseInput= document.getElementById('Salad-number');
+        var caseNumber= caseInput.value;
+        caseInput.value=parseInt(caseNumber)+1;
+        
+        
+        
+        })
+
+
+// Cloth section
+
+
+ //Blue-polo
+ document.getElementById('Blue-polo-plus').addEventListener('click',function(){
+    var caseInput= document.getElementById('Blue-polo-number');
+    var caseNumber= caseInput.value;
+    caseInput.value=parseInt(caseNumber)+1;
+    
+    
+    
+    })
+
+
+
+ //USPA
+     document.getElementById('USPA-plus').addEventListener('click',function(){
+        var caseInput= document.getElementById('USPA-number');
+        var caseNumber= caseInput.value;
+        caseInput.value=parseInt(caseNumber)+1;
+        
+        
+        
+        })
+
+
+
+ //Blue-polo1
+    document.getElementById('Blue-polo1-plus').addEventListener('click',function(){
+        var caseInput= document.getElementById('Blue-polo1-number');
+        var caseNumber= caseInput.value;
+        caseInput.value=parseInt(caseNumber)+1;
+        
+        
+        
+        })
+
+// Toys corner
+
+
+// Whole-sale
+
+document.getElementById('Whole-sale-plus').addEventListener('click',function(){
+    var caseInput= document.getElementById('Whole-sale-number');
+    var caseNumber= caseInput.value;
+    caseInput.value=parseInt(caseNumber)+1;
+    
+    
+    
+    })
+
+
+
+// Spider Man
+
+document.getElementById('Spider-plus').addEventListener('click',function(){
+    var caseInput= document.getElementById('Spider-number');
+    var caseNumber= caseInput.value;
+    caseInput.value=parseInt(caseNumber)+1;
+    
+    
+    
+    })
+
+
+
+//Truck
+document.getElementById('Truck-plus').addEventListener('click',function(){
+    var caseInput= document.getElementById('Truck-number');
+    var caseNumber= caseInput.value;
+    caseInput.value=parseInt(caseNumber)+1;
+    
+    
+    
+    })
