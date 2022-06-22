f8f3e652620a57b8bf364241a631ec8c8f565c2f
Apurbo73 <apurbodebnath895@gmail.com>
2022-06-22 16:04:37 +0600

    add remove only

--- javascript.js
+++ javascript.js
@@ -1,279 +1,503 @@
 //smart phone 
-//Xiomi
-document.getElementById('Xiomi-plus').addEventListener('click',function(){
-var caseInput= document.getElementById('Xiomi-number');
-var caseNumber= caseInput.value;
-caseInput.value=parseInt(caseNumber)+1;
+//Xiomi adding
+document.getElementById('Xiomi-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Xiomi-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
 
 
+});
 
-})
 
+//Xiomi removing
 
+document.getElementById('Xiomi-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Xiomi-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+});
 
 
 //Iphone
 
-document.getElementById('Iphone-plus').addEventListener('click',function(){
-    var caseInput= document.getElementById('Iphone-number');
-    var caseNumber= caseInput.value;
-    caseInput.value=parseInt(caseNumber)+1;
-    
-    
-    
-    })
+document.getElementById('Iphone-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Iphone-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
 
+})
+//Iphone removing
+
+document.getElementById('Iphone-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Iphone-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+})
 
 
 //Samsung
 
-document.getElementById('Samsung-plus').addEventListener('click',function(){
-    var caseInput= document.getElementById('Samsung-number');
-    var caseNumber= caseInput.value;
-    caseInput.value=parseInt(caseNumber)+1;
-    
-    
-    
-    })
+document.getElementById('Samsung-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Samsung-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
+
+
+
+});
+
+//Samsung removing
+
+document.getElementById('Samsung-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Samsung-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+});
 
 
 
 
+//Laptops
+
+//Lenovo
+
+document.getElementById('Lenovo-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Lenovo-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
 
 
 
-    //Laptops
+});
 
-    //Lenovo
+//Lenovo removing
 
-    document.getElementById('Lenovo-plus').addEventListener('click',function(){
-        var caseInput= document.getElementById('Lenovo-number');
-        var caseNumber= caseInput.value;
-        caseInput.value=parseInt(caseNumber)+1;
-        
-        
-        
-        })
+document.getElementById('Lenovo-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Lenovo-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
 
-
+    }
+});
 
 
-    //Dell
 
-    document.getElementById('Dell-plus').addEventListener('click',function(){
-        var caseInput= document.getElementById('Dell-number');
-        var caseNumber= caseInput.value;
-        caseInput.value=parseInt(caseNumber)+1;
-        
-        
-        
-        })
+//Dell adding
+
+document.getElementById('Dell-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Dell-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
 
 
-    // Mac
 
-    document.getElementById('Mac-plus').addEventListener('click',function(){
-        var caseInput= document.getElementById('Mac-number');
-        var caseNumber= caseInput.value;
-        caseInput.value=parseInt(caseNumber)+1;
-        
-        
-        
-        })
+});
+
+
+//Dell removing
+
+document.getElementById('Dell-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Dell-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+});
+
+
+
+// Mac adding
+
+document.getElementById('Mac-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Mac-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
+
+
+
+});
+//Mac removing
+
+document.getElementById('Mac-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Mac-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+});
 
 
 
 //Shoes corner
 
-//Sply
+//Sply adding
 
-document.getElementById('Sply-plus').addEventListener('click',function(){
-    var caseInput= document.getElementById('Sply-number');
-    var caseNumber= caseInput.value;
-    caseInput.value=parseInt(caseNumber)+1;
-    
-    
-    
-    })
+document.getElementById('Sply-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Sply-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
 
 
+});
 
-// Nike
+//Sply removing
+
+document.getElementById('Sply-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Sply-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+});
 
 
-document.getElementById('Nike-plus').addEventListener('click',function(){
-    var caseInput= document.getElementById('Nike-number');
-    var caseNumber= caseInput.value;
-    caseInput.value=parseInt(caseNumber)+1;
-    
-    
-    
-    })
+// Nike adding
+
+
+document.getElementById('Nike-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Nike-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
 
 
 
+})
+
+//Nike removing
+
+document.getElementById('Nike-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Nike-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+});
 
 
 
 
 //Red Airmax
 
-document.getElementById('Red-plus').addEventListener('click',function(){
-    var caseInput= document.getElementById('Red-number');
-    var caseNumber= caseInput.value;
-    caseInput.value=parseInt(caseNumber)+1;
-    
-    
-    
-    })
+document.getElementById('Red-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Red-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
 
 
 
-    //Bags corner
+})
+//Red removing
 
- //Red-bag
-document.getElementById('Red-bag-plus').addEventListener('click',function(){
-    var caseInput= document.getElementById('Red-bag-number');
-    var caseNumber= caseInput.value;
-    caseInput.value=parseInt(caseNumber)+1;
-    
-    
-    
-    })
+document.getElementById('Red-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Red-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+});
+
+
+//Bags corner
+
+//Red-bag
+document.getElementById('Red-bag-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Red-bag-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
+
+
+
+});
+
+//Red-bag removing
+
+document.getElementById('Red-bag-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Red-bag-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+});
+
 
 
 // Blue bag
-document.getElementById('Blue-bag-plus').addEventListener('click',function(){
-    var caseInput= document.getElementById('Blue-bag-number');
-    var caseNumber= caseInput.value;
-    caseInput.value=parseInt(caseNumber)+1;
-    
-    
-    
-    })
-
-
-  // Black bag
-    document.getElementById('Black-bag-plus').addEventListener('click',function(){
-        var caseInput= document.getElementById('Black-bag-number');
-        var caseNumber= caseInput.value;
-        caseInput.value=parseInt(caseNumber)+1;
-        
-        
-        
-        })
+document.getElementById('Blue-bag-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Blue-bag-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
 
 
 
+});
+
+//Blue bag removing
+
+document.getElementById('Blue-bag-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Blue-bag-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+});
 
 
-        //Foods corner
+
+// Black bag
+document.getElementById('Black-bag-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Black-bag-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
+
+
+
+})
+
+//Black bag removing
+
+document.getElementById('Black-bag-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Black-bag-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+});
+
+
+
+//Foods corner
 // Burger
-document.getElementById('Burger-plus').addEventListener('click',function(){
-    var caseInput= document.getElementById('Burger-number');
-    var caseNumber= caseInput.value;
-    caseInput.value=parseInt(caseNumber)+1;
-    
-    
-    
-    })
+document.getElementById('Burger-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Burger-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
+
+
+
+});
+
+
+//Burger removing
+
+document.getElementById('Burger-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Burger-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+});
 
 
 //Combo
-document.getElementById('Combo-plus').addEventListener('click',function(){
-    var caseInput= document.getElementById('Combo-number');
-    var caseNumber= caseInput.value;
-    caseInput.value=parseInt(caseNumber)+1;
-    
-    
-    
-    })
+document.getElementById('Combo-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Combo-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
 
 
 
- //Salad
-    document.getElementById('Salad-plus').addEventListener('click',function(){
-        var caseInput= document.getElementById('Salad-number');
-        var caseNumber= caseInput.value;
-        caseInput.value=parseInt(caseNumber)+1;
-        
-        
-        
-        })
+});
+
+//combo removing
+
+document.getElementById('Combo-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Combo-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+});
+
+
+
+//Salad
+document.getElementById('Salad-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Salad-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
+
+
+
+});
+
+//Salad removing
+
+document.getElementById('Salad-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Salad-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+});
+
 
 
 // Cloth section
 
 
- //Blue-polo
- document.getElementById('Blue-polo-plus').addEventListener('click',function(){
-    var caseInput= document.getElementById('Blue-polo-number');
-    var caseNumber= caseInput.value;
-    caseInput.value=parseInt(caseNumber)+1;
-    
-    
-    
-    })
+//Blue-polo
+document.getElementById('Blue-polo-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Blue-polo-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
 
 
 
- //USPA
-     document.getElementById('USPA-plus').addEventListener('click',function(){
-        var caseInput= document.getElementById('USPA-number');
-        var caseNumber= caseInput.value;
-        caseInput.value=parseInt(caseNumber)+1;
-        
-        
-        
-        })
+});
+//Blue polo removing
+
+document.getElementById('Blue-polo-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Blue-polo-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+});
 
 
 
- //Blue-polo1
-    document.getElementById('Blue-polo1-plus').addEventListener('click',function(){
-        var caseInput= document.getElementById('Blue-polo1-number');
-        var caseNumber= caseInput.value;
-        caseInput.value=parseInt(caseNumber)+1;
-        
-        
-        
-        })
+//USPA
+document.getElementById('USPA-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('USPA-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
+
+
+
+});
+
+//USPA removing
+
+document.getElementById('USPA-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('USPA-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+});
+
+
+
+
+//Blue-polo1
+document.getElementById('Blue-polo1-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Blue-polo1-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
+
+
+
+});
+
+
+//Blue-polo1 removing
+document.getElementById('Blue-polo1-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Blue-polo1-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+
+
+
+});
 
 // Toys corner
 
 
 // Whole-sale
 
-document.getElementById('Whole-sale-plus').addEventListener('click',function(){
-    var caseInput= document.getElementById('Whole-sale-number');
-    var caseNumber= caseInput.value;
-    caseInput.value=parseInt(caseNumber)+1;
-    
-    
-    
-    })
+document.getElementById('Whole-sale-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Whole-sale-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
+
+
+
+});
+//Whole sale removing
+document.getElementById('Whole-sale-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Whole-sale-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+
+
+
+});
 
 
 
 // Spider Man
 
-document.getElementById('Spider-plus').addEventListener('click',function(){
-    var caseInput= document.getElementById('Spider-number');
-    var caseNumber= caseInput.value;
-    caseInput.value=parseInt(caseNumber)+1;
-    
-    
-    
-    })
+document.getElementById('Spider-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Spider-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
+
+
+
+});
+//Spider man removing
+document.getElementById('Spider-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Spider-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+
+
+
+});
 
 
 
 //Truck
-document.getElementById('Truck-plus').addEventListener('click',function(){
-    var caseInput= document.getElementById('Truck-number');
-    var caseNumber= caseInput.value;
-    caseInput.value=parseInt(caseNumber)+1;
-    
-    
-    
-    })
+document.getElementById('Truck-plus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Truck-number');
+    var caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) + 1;
+
+
+
+});
+
+//Truck removing
+document.getElementById('Truck-minus').addEventListener('click', function() {
+    var caseInput = document.getElementById('Truck-number');
+    var caseNumber = caseInput.value;
+    if (caseNumber > 0) {
+        caseInput.value = parseInt(caseNumber) - 1;
+
+    }
+
+
+
+});
